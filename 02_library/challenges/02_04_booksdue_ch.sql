682db5aee06e11f0caeffe09b248eaae437188ac
dkhawthorne <dakotakh24@gmail.com>
2024-10-24 08:34:14 -0500

    Update 02_04_booksdue_ch.sql

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,7 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT * 
+FROM Loans
+WHERE DueDate = '2022-07-13';
