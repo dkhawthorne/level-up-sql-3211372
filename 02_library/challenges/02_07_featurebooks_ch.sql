80ab75b24bd42999d2739315fdc5a46b9b5eee9b
dkhawthorne <dakotakh24@gmail.com>
2024-10-24 09:08:58 -0500

    Update 02_07_featurebooks_ch.sql

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,15 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+
+SELECT
+  Title,
+  Author,
+  Published
+FROM Books
+WHERE Published BETWEEN 1890 AND 1899
+AND (BookID NOT IN
+  (SELECT BookID
+  FROM Loans
+  WHERE ReturnedDate IS NULL));
