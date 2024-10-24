5f602e9cbae6ab4553f866b3c2de6935321c9520
dkhawthorne <dakotakh24@gmail.com>
2024-10-24 09:19:13 -0500

    Update 02_08_bookstats_ch.sql

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -1,7 +1,25 @@
 -- Create two reports about book statistics.
 
 -- Report 1: Show how many titles were published 
--- in each year.
+-- in each year. Sorted by years with the most books published at the top
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+
+SELECT 
+  COUNT(DISTINCT(Title)) as NumberBooks,
+  Published
+FROM Books
+GROUP BY Published
+ORDER BY NumberBooks DESC;
+
+SELECT
+  COUNT(l.LoanIN) as LoanCount
+  b.Title,
+  b.Author
+FROM Books b
+JOIN Loans l ON b.BookID = l.BookID
+GROUP BY b.Title
+ORDER BY LoanCount DESC
+LIMIT 5;
