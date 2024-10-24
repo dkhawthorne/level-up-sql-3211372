56118b034c5e6c109a65897de431389138f7b162
dkhawthorne <dakotakh24@gmail.com>
2024-10-24 08:55:35 -0500

    Update 02_06_visitreminder_ch.sql

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,13 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT 
+  COUNT(l.LoanID) as LoanCount,
+   p.FirstName,
+   p.LastName,
+   p.Email
+FROM Loans l
+JOIN Patrons p ON l.PatronID = p.PatronID
+GROUP BY l.PatronID
+ORDER BY LoanCount
+LIMIT 20;
