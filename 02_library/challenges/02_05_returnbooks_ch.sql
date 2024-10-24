ee8841e432168c89b650c4b5feb170d569c96a45
dkhawthorne <dakotakh24@gmail.com>
2024-10-24 08:44:17 -0500

    Update 02_05_returnbooks_ch.sql

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,19 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+
+UPDATE Loans
+SET Loans.ReturnedDate = '2022-07-05'
+WHERE Loans.BookID = (SELECT Books.BookID FROM Books WHERE Barcode = 6435968624)
+AND Loans.ReturnedDate IS NULL;
+
+UPDATE Loans
+SET Loans.ReturnedDate = '2022-07-05'
+WHERE Loans.BookID = (SELECT Books.BookID FROM Books WHERE Barcode = 5677520613)
+AND Loans.ReturnedDate IS NULL;
+
+UPDATE Loans
+SET Loans.ReturnedDate = '2022-07-05'
+WHERE Loans.BookID = (SELECT Books.BookID FROM Books WHERE Barcode = 8730298424)
+AND Loans.ReturnedDate IS NULL;
